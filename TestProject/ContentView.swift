3793cb2193eaaa3fa56c202da8abb6fbfa1492dd
edgarkosyan <e.kosyan0073@gmail.com>
2022-12-22 16:07:13 +0400

    first Commit

--- TestProject/ContentView.swift
+++ TestProject/ContentView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+	@ObservedObject var vm: ContentViewModel = ContentViewModel()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+			Text(vm.text)
+            
         }
         .padding()
     }
---
+++ TestProject/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  TestProject
+//
+//  Created by Edgar on 22.12.22.
+//
+
+import Foundation
+
+class ContentViewModel: ObservableObject {
+	@Published var text: String = "Hello Chigsa"
+}
